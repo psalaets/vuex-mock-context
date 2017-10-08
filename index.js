447a2db5cd5058061810822f42be9377574ef679
Paul Salaets <psalaets@gmail.com>
2017-10-08 16:47:40 -0400

    semi big restructure, don't try to normalize payload

--- index.js
+++ index.js
@@ -8,33 +8,10 @@ export function create() {
     state: {},
     rootState: {},
     dispatch(...args) {
-      log.push({
-        dispatch: normalizePayload(...args)
-      });
+      log.push({dispatch: args});
     },
     commit(...args) {
-      log.push({
-        commit: normalizePayload(...args)
-      });
+      log.push({commit: args});
     }
   };
-}
-
-export function normalizePayload(...args) {
-  let parts;
-
-  if (args.length == 1) {
-    if (typeof args[0] == 'string') {
-      // just a type string
-      parts = [args[0]];
-    } else {
-      // type is property in payload object
-      parts = [args[0].type, args[0]];
-    }
-  } else {
-    // type and payload are separate args
-    parts = args.slice();
-  }
-
-  return parts;
 }
--- test.js
+++ test.js
@@ -1,7 +1,149 @@
 const test = require('tape');
-const {create, normalizePayload} = require('./dist/index.js');
+const {create} = require('./dist/index.js');
 
-test('dispatch action', (t) => {
+test('dispatch', t => {
+  t.test('action with no payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch('loadRecord');
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: ['loadRecord']}]
+    );
+  });
+
+  t.test('action with payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch('loadRecord', {id: 5});
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: ['loadRecord', {id: 5}]}]
+    );
+  });
+
+  t.test('action with type in payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch({type: 'loadRecord', id: 5});
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: [{type: 'loadRecord', id: 5}]}]
+    );
+  });
+
+  t.test('root action with no payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch('loadRecord', null, {root: true});
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: ['loadRecord', null, {root: true}]}]
+    );
+  });
+
+  t.test('root action with payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch('loadRecord', {id: 5}, {root: true});
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: ['loadRecord', {id: 5}, {root: true}]}]
+    );
+  });
+
+  t.test('root action with type in payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.dispatch({type: 'loadRecord', id: 5}, null, {root: true});
+
+    st.deepEqual(
+      mockContext.log,
+      [{dispatch: [{type: 'loadRecord', id: 5}, null, {root: true}]}]
+    );
+  });
+});
+
+test('commit', t => {
+  t.test('mutation with no payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit('changeState');
+
+    st.deepEqual(mockContext.log, [
+      {commit: ['changeState']}
+    ]);
+  });
+
+  t.test('mutation with payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit('changeState', {id: 5});
+
+    st.deepEqual(mockContext.log, [
+      {commit: ['changeState', {id: 5}]}
+    ]);
+  });
+
+  t.test('mutation with type in payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit({type: 'changeState', id: 5});
+
+    st.deepEqual(mockContext.log, [
+      {commit: [{type: 'changeState', id: 5}]}
+    ]);
+  });
+
+  t.test('root mutation with no payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit('changeState', null, {root: true});
+
+    st.deepEqual(mockContext.log, [
+      {commit: ['changeState', null, {root: true}]}
+    ]);
+  });
+
+  t.test('root mutation with payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit('changeState', {id: 5}, {root: true});
+
+    st.deepEqual(mockContext.log, [
+      {commit: ['changeState', {id: 5}, {root: true}]}
+    ]);
+  });
+
+  t.test('root mutation with type in payload', st => {
+    st.plan(1);
+    const mockContext = create();
+
+    mockContext.commit({type: 'changeState', id: 5}, {root: true});
+
+    st.deepEqual(mockContext.log, [
+      {commit: [{type: 'changeState', id: 5}, {root: true}]}
+    ]);
+  });
+});
+
+test('dispatches mixed with commits', (t) => {
   t.plan(1);
   const mockContext = create();
 
@@ -10,146 +152,47 @@ test('dispatch action', (t) => {
     blah: 'foo',
   });
 
-  t.deepEqual(mockContext.log, [
-    {dispatch: ['loadRecord', {
-      id: 5,
-      blah: 'foo'
-    }]}
-  ]);
-});
-
-test('dispatch action with primitive payload', (t) => {
-  t.plan(1);
-  const mockContext = create();
-
-  mockContext.dispatch('loadRecord', 5);
-
-  t.deepEqual(mockContext.log, [
-    {dispatch: ['loadRecord', 5]}
-  ]);
-});
-
-test('commit mutation', (t) => {
-  t.plan(1);
-  const mockContext = create();
-
   mockContext.commit('changeState', {
     id: 1
   });
 
-  t.deepEqual(mockContext.log, [
-    {commit: ['changeState', {
-      id: 1
-    }]}
-  ]);
-});
-
-test('commit mutation with primitive payload', (t) => {
-  t.plan(1);
-  const mockContext = create();
-
-  mockContext.commit('changeState', 'dog');
-
-  t.deepEqual(mockContext.log, [
-    {commit: ['changeState', 'dog']}
-  ]);
-});
-
-test('actions and mutations', (t) => {
-  t.plan(1);
-  const mockContext = create();
-
-  mockContext.dispatch('loadRecord', {
-    id: 5,
-    blah: 'foo',
-  });
-
-  mockContext.commit('changeState', {
-    id: 1
-  });
-
-  mockContext.commit('changeStateAgain', {
+  mockContext.commit('anotherChange', {
     id: 10
   });
 
   t.deepEqual(mockContext.log, [
-    {dispatch: ['loadRecord', {
-      id: 5,
-      blah: 'foo'
-    }]},
-    {commit: ['changeState', {
-      id: 1
-    }]},
-    {commit: ['changeStateAgain', {
-      id: 10
-    }]}
+    {dispatch: ['loadRecord', {id: 5, blah: 'foo'}]},
+    {commit: ['changeState', {id: 1}]},
+    {commit: ['anotherChange', {id: 10}]}
   ]);
 });
 
-test('context has empty getters object', (t) => {
-  t.plan(1);
-  const mockContext = create();
+test('other context properties', t => {
+  t.test('getters is empty object', st => {
+    st.plan(1);
+    const mockContext = create();
 
-  t.deepEqual(mockContext.getters, {});
-});
+    st.deepEqual(mockContext.getters, {});
+  });
 
-test('context has empty state object', (t) => {
-  t.plan(1);
-  const mockContext = create();
+  t.test('state is empty object', st => {
+    st.plan(1);
+    const mockContext = create();
 
-  t.deepEqual(mockContext.state, {});
-});
+    st.deepEqual(mockContext.state, {});
+  });
 
-test('context has empty rootGetters object', (t) => {
-  t.plan(1);
-  const mockContext = create();
+  t.test('rootGetters is empty object', st => {
+    st.plan(1);
+    const mockContext = create();
 
-  t.deepEqual(mockContext.rootGetters, {});
-});
+    st.deepEqual(mockContext.rootGetters, {});
+  });
 
-test('context has empty rootState object', (t) => {
-  t.plan(1);
-  const mockContext = create();
+  t.test('rootState is empty object', st => {
+    st.plan(1);
+    const mockContext = create();
 
-  t.deepEqual(mockContext.rootState, {});
-});
-
-test('normalizePayload with type only', (t) => {
-  t.plan(1);
-
-  const result = normalizePayload('loadRecord');
-
-  t.deepEqual(result, ['loadRecord']);
-});
-
-test('normalizePayload with everything in payload', (t) => {
-  t.plan(1);
-
-  const payload = {
-    type: 'loadRecord',
-    id: 5,
-    blah: 'foo'
-  };
-  const result = normalizePayload(payload);
-
-  t.deepEqual(result, ['loadRecord', {
-    type: 'loadRecord',
-    id: 5,
-    blah: 'foo'
-  }]);
-});
-
-test('normalizePayload with type and payload', (t) => {
-  t.plan(1);
-
-  const payload = {
-    id: 5,
-    blah: 'foo'
-  };
-  const result = normalizePayload('loadRecord', payload);
-
-  t.deepEqual(result, ['loadRecord', {
-    id: 5,
-    blah: 'foo'
-  }]);
+    st.deepEqual(mockContext.rootState, {});
+  });
 });
